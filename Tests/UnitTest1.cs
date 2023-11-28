c4ea6bd52a1ffb44faf405fa1ba82a8958f81aea
Chumam <chumam@hollywoodbets.net>
2023-11-28 15:22:44 +0200

    asas

--- Tests/UnitTest1.cs
+++ Tests/UnitTest1.cs
@@ -23,7 +23,7 @@ namespace Tests
             int x = 4;
             int y = 6;
             int test = 1;
-            test = 2;
+            test = 22;
             // Act
             int result = x * y +test;
 
